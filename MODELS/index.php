98ad2d203bd5765619921276ba1a3f40bcb49b42
Andsirpa <andsirpa94@gmail.com>
2024-03-10 12:16:15 +0100

    add set_title and get_title functions

--- MODELS/index.php
+++ MODELS/index.php
@@ -12,4 +12,20 @@ class Production
         $this->language = $language;
         $this->rating = $rating;
     }
+
+    public static function set_title($title)
+    {
+        // controllo che ci sia il titolo
+        if (empty($title))
+            return;
+
+        // mi assicuro che la prima lettera sia maiuscola
+        $this->title = ucfirst($title);
+
+    }
+
+    public static function get_title()
+    {
+        return $this->title;
+    }
 }
